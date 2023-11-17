a7d36e5ac19aa06318fbeaf67abea426978bd85c
Hoa Vuong <hoa.vuong_ext@doctoranywhere.com>
2023-11-17 21:34:28 +0700

    day 14, refractor variable name in day-13

--- src/main/java/api_learning/FormInteractionMultipleMatching.java
+++ src/main/java/api_learning/FormInteractionMultipleMatching.java
@@ -10,7 +10,7 @@ import java.util.NoSuchElementException;
 
 public class FormInteractionMultipleMatching {
     private static final String TARGET_URL = "https://the-internet.herokuapp.com/login";
-    private static final By LOGIN_INPUT_FIELDS_SEL = (By.tagName("input"));
+    private static final By INPUT_FIELDS_SEL = (By.tagName("input"));
     private static final By LOGIN_BTN_SEL = (By.cssSelector("#login [type='submit']"));
 
     public static void main(String[] args) {
@@ -19,17 +19,18 @@ public class FormInteractionMultipleMatching {
         driver.get(TARGET_URL);
 
         try {
-           List<WebElement> loginBtnEles = driver.findElements(LOGIN_INPUT_FIELDS_SEL);
-           if (!loginBtnEles.isEmpty()){
+           List<WebElement> inputFieldEles = driver.findElements(INPUT_FIELDS_SEL);
+           if (!inputFieldEles.isEmpty()){
                final int USERNAME_INDEX = 0;
                final int PASSWORD_INDEX = 1;
-               loginBtnEles.get(USERNAME_INDEX).sendKeys("hoa@gmail.com");
-               loginBtnEles.get(PASSWORD_INDEX).sendKeys("123123");
+               inputFieldEles.get(USERNAME_INDEX).sendKeys("hoa@gmail.com");
+               inputFieldEles.get(PASSWORD_INDEX).sendKeys("123123");
            } else {
                throw new NoSuchElementException("No login fields found!");
            }
 
            driver.findElement(LOGIN_BTN_SEL).click();
+           Thread.sleep(2000);
 
         } catch (Exception e) {
             e.printStackTrace();
